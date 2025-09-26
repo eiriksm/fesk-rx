bf2f0d4a33b361fd8784153aff5ca56a046cf965
Eirik S. Morland <eirik@morland.no>
2025-09-26 19:44:53 +0200

    Uncomment

--- webapp/cypress/e2e/fesk-decoder.cy.js
+++ webapp/cypress/e2e/fesk-decoder.cy.js
@@ -46,37 +46,37 @@ describe('FESK Decoder Test Suite', () => {
     })
   })
 
-  // it('should successfully decode fesk1.wav', () => {
-  //   const testFile = testFiles[0] // fesk1.wav
-  //   cy.testFeskFile(
-  //     0,
-  //     testFile.expectedMessage
-  //   )
-  // })
+  it('should successfully decode fesk1.wav', () => {
+    const testFile = testFiles[0] // fesk1.wav
+    cy.testFeskFile(
+      0,
+      testFile.expectedMessage
+    )
+  })
 
-  // it('should successfully decode fesk2.wav', () => {
-  //   const testFile = testFiles[1] // fesk2.wav
-  //   cy.testFeskFile(
-  //     1,
-  //     testFile.expectedMessage
-  //   )
-  // })
+  it('should successfully decode fesk2.wav', () => {
+    const testFile = testFiles[1] // fesk2.wav
+    cy.testFeskFile(
+      1,
+      testFile.expectedMessage
+    )
+  })
 
-  // it('should successfully decode fesk3.wav', () => {
-  //   const testFile = testFiles[2] // fesk3.wav
-  //   cy.testFeskFile(
-  //     2,
-  //     testFile.expectedMessage,
-  //   )
-  // })
+  it('should successfully decode fesk3.wav', () => {
+    const testFile = testFiles[2] // fesk3.wav
+    cy.testFeskFile(
+      2,
+      testFile.expectedMessage,
+    )
+  })
 
-  // it('should successfully decode fesk1mp.wav', () => {
-  //   const testFile = testFiles[3] // fesk1mp.wav
-  //   cy.testFeskFile(
-  //     3,
-  //     testFile.expectedMessage,
-  //   )
-  // })
+  it('should successfully decode fesk1mp.wav', () => {
+    const testFile = testFiles[3] // fesk1mp.wav
+    cy.testFeskFile(
+      3,
+      testFile.expectedMessage,
+    )
+  })
 
   it('should successfully decode webapp-fesk1.wav', () => {
     const testFile = testFiles[4] // webapp-fesk1.wav
